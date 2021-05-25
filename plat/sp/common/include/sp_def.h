33f841f12237f2bef19f4b1725a86c3fe2e95205
Wells Lu <wells.lu@sunplus.com>
2021-05-25 23:17:04 +0800

    [Q645] Revised SP_ARM_COUNTER_CLK_IN: 27M --> 25M

--- plat/sp/common/include/sp_def.h
+++ plat/sp/common/include/sp_def.h
@@ -7,11 +7,11 @@
 #ifndef SP_DEF_H
 #define SP_DEF_H
 
-#define SP_ARM_COUNTER_CLK_IN		27000000 /* 27MHz */
+#define SP_ARM_COUNTER_CLK_IN		25000000 /* 25MHz */
 
-#define SP_UART0_BAUDRATE			921600
-#define SP_UART0_CLK_IN_HZ			(216 * 1000 * 1000)
+#define SP_UART0_BAUDRATE		921600
+#define SP_UART0_CLK_IN_HZ		(216 * 1000 * 1000)
 
-#define SP_Q645						0x00000630 /* G0.0 */
+#define SP_Q645				0x00000630 /* G0.0 */
 
 #endif /* SP_DEF_H */
