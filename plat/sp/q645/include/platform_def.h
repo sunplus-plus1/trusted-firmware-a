48f138151bb2178ad85cfbe9e2d669ee0eea55e3
Wells Lu <wellslutw@gmail.com>
2022-01-28 12:31:48 +0800

    [Q645] Changed SP_LINUX_DTB_OFFSET to 0x780000.

--- plat/sp/q645/include/platform_def.h
+++ plat/sp/q645/include/platform_def.h
@@ -13,7 +13,7 @@
 
 #include <sp_mmap.h>
 
-#define CPU_WAIT_INIT_VAL			0xffffffff
+#define CPU_WAIT_INIT_VAL		0xffffffff
 #define CORE_CPU_START_POS(core_id)	(CORE0_CPU_START_POS - ((core_id) * 8))
 #define CORE3_CPU_START_POS     	(0xfa23fc00 - 0x28)  // core3 wait fa23_fbd8
 #define CORE2_CPU_START_POS    	 	(0xfa23fc00 - 0x20)  // core2 wait fa23_fbe0
@@ -35,10 +35,10 @@
 #define SP_DRAM_NS_ADDR			(SP_DRAM_SEC_ADDR + SP_DRAM_SEC_SIZE)
 #define SP_DRAM_NS_SIZE			((64U << 20) - SP_DRAM_NS_ADDR)
 
-#define BL31_BASE				0x200000 /* @ 2MB */
-#define BL31_LIMIT				(BL31_BASE + 0x100000)
+#define BL31_BASE			0x200000 /* @ 2MB */
+#define BL31_LIMIT			(BL31_BASE + 0x100000)
 
-#define SP_LINUX_DTB_OFFSET		(SP_DRAM_BASE + 0x3f0000)  /* dtb @ 4MB */
+#define SP_LINUX_DTB_OFFSET		(SP_DRAM_BASE + 0x780000)  /* dtb @ 7M+512k */
 #define PLAT_SP_NS_IMAGE_OFFSET	(SP_DRAM_BASE + 0x300040) /* uboot @ 3MB+64 */
 
 
@@ -53,8 +53,8 @@
 
 /* map regions */
 #define PLATFORM_MMAP_REGIONS		4
-#define MAX_MMAP_REGIONS			(3 + PLATFORM_MMAP_REGIONS)
-#define MAX_XLAT_TABLES				8
+#define MAX_MMAP_REGIONS		(3 + PLATFORM_MMAP_REGIONS)
+#define MAX_XLAT_TABLES			8
 
 #define PLAT_PHY_ADDR_SPACE_SIZE	(1ULL << 32)
 #define PLAT_VIRT_ADDR_SPACE_SIZE	(1ULL << 32)
@@ -62,14 +62,14 @@
 
 /* PWR */
 #define PLAT_MAX_PWR_LVL_STATES		U(2)
-#define PLAT_MAX_RET_STATE			U(1)
-#define PLAT_MAX_OFF_STATE			U(2)
-#define PLAT_MAX_PWR_LVL			U(2) /* See plat/sp/common/sp_topology.c */
+#define PLAT_MAX_RET_STATE		U(1)
+#define PLAT_MAX_OFF_STATE		U(2)
+#define PLAT_MAX_PWR_LVL		U(2) /* See plat/sp/common/sp_topology.c */
 #define PLAT_NUM_PWR_DOMAINS		(U(1) + PLATFORM_CLUSTER_COUNT + PLATFORM_CORE_COUNT)
 
 /* cores */
-#define PLATFORM_CLUSTER_COUNT			U(1)
+#define PLATFORM_CLUSTER_COUNT		U(1)
 #define PLATFORM_MAX_CPUS_PER_CLUSTER	U(4)
-#define PLATFORM_CORE_COUNT				(PLATFORM_CLUSTER_COUNT * PLATFORM_MAX_CPUS_PER_CLUSTER)
+#define PLATFORM_CORE_COUNT		(PLATFORM_CLUSTER_COUNT * PLATFORM_MAX_CPUS_PER_CLUSTER)
 
 #endif /* PLATFORM_DEF_H */
