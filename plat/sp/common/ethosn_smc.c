0e3120cd5271cc0ea0b070a73c5b1aceb765f5c7
Akira.Yu <akira.yu@sunplus>
2022-03-03 11:18:39 +0800

    [TFA] Modification for cooperating with EthosN-N78 linux kernel module 21.11

--- plat/sp/common/ethosn_smc.c
+++ plat/sp/common/ethosn_smc.c
@@ -16,11 +16,10 @@
 
 #define FCONF_GET_PROPERTY(a, b, c)	a##__##b##_getter(c)
 
-/* Arm Ethos-N NPU (NPU) status */
-#define ETHOSN_STATUS \
-	FCONF_GET_PROPERTY(hw_config, ethosn_config, status)
-
-/* Number of NPU cores available */
+/*
+ * Number of Arm Ethos-N NPU (NPU) cores available for a
+ * particular parent device
+ */
 #define ETHOSN_NUM_CORES \
 	FCONF_GET_PROPERTY(hw_config, ethosn_config, num_cores)
 
@@ -53,6 +52,17 @@
 #define SEC_SYSCTRL0_SOFT_RESET		U(3U << 29)
 #define SEC_SYSCTRL0_HARD_RESET		U(1U << 31)
 
+static bool ethosn_is_core_addr_valid(uintptr_t core_addr)
+{
+	for (uint32_t core_idx = 0U; core_idx < ETHOSN_NUM_CORES; core_idx++) {
+		if (ETHOSN_CORE_ADDR(core_idx) == core_addr) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
 static void ethosn_delegate_to_ns(uintptr_t core_addr)
 {
 	mmio_setbits_32(ETHOSN_CORE_SEC_REG(core_addr, SEC_SECCTLR_REG),
@@ -68,9 +78,9 @@ static void ethosn_delegate_to_ns(uintptr_t core_addr)
 			SEC_DEL_ADDR_EXT_VAL);
 }
 
-static int ethosn_is_sec(void)
+static int ethosn_is_sec(uintptr_t core_addr)
 {
-	if ((mmio_read_32(ETHOSN_CORE_SEC_REG(ETHOSN_CORE_ADDR(0), SEC_DEL_REG))
+	if ((mmio_read_32(ETHOSN_CORE_SEC_REG(core_addr, SEC_DEL_REG))	
 		& SEC_DEL_EXCC_MASK) != 0U) {
 		return 0;
 	}
@@ -105,15 +115,15 @@ static bool ethosn_reset(uintptr_t core_addr, int hard_reset)
 	NOTICE("HW sec control(0xF80029E8):(%x)\n",mmio_read_32(0xF80029E8));//mon hw sec n78:bit9,bit1
 #endif
 #if 1//enable HW auto reset
-    NOTICE("HW reset control(0xF8000164):(%x)\n",mmio_read_32(0xF8000164));
+    //NOTICE("HW reset control(0xF8000164):(%x)\n",mmio_read_32(0xF8000164));
     mmio_write_32(0xF8000164, 0x2000|(0x2000<<16));//HW auto reset enable (bit13)
     udelay(1);
-	NOTICE("HW reset control(0xF8000164):(%x)\n",mmio_read_32(0xF8000164));
+	//NOTICE("HW reset control(0xF8000164):(%x)\n",mmio_read_32(0xF8000164));
 #endif
 
 	mmio_write_32(sysctrl0_reg, reset_val);//n78 sw reset
 
-	NOTICE("HW reset control(0xF8000164):(%x)\n",mmio_read_32(0xF8000164));
+	//NOTICE("HW reset control(0xF8000164):(%x)\n",mmio_read_32(0xF8000164));
 
 #if 0//enable SW monitor reset
 	/* Wait for N78 resetreq(0xF8000164.bit12 to be 1. then it can reset N78 manually with Moon N78 reset reg (0xF800005C.0) */
@@ -168,7 +178,7 @@ static bool ethosn_reset(uintptr_t core_addr, int hard_reset)
 }
 
 uintptr_t ethosn_smc_handler(uint32_t smc_fid,
-			     u_register_t core_idx,
+                 u_register_t core_addr,
 			     u_register_t x2,
 			     u_register_t x3,
 			     u_register_t x4,
@@ -176,8 +186,8 @@ uintptr_t ethosn_smc_handler(uint32_t smc_fid,
 			     void *handle,
 			     u_register_t flags)
 {
-	uintptr_t core_addr;
 	int hard_reset = 0;
+	const uint32_t fid = smc_fid & FUNCID_NUM_MASK;
 
 	/* Only SiP fast calls are expected */
 	if ((GET_SMC_TYPE(smc_fid) != SMC_TYPE_FAST) ||
@@ -187,7 +197,7 @@ uintptr_t ethosn_smc_handler(uint32_t smc_fid,
 
 	/* Truncate parameters to 32-bits for SMC32 */
 	if (GET_SMC_CC(smc_fid) == SMC_32) {
-		core_idx &= 0xFFFFFFFF;
+        core_addr &= 0xFFFFFFFF;
 		x2 &= 0xFFFFFFFF;
 		x3 &= 0xFFFFFFFF;
 		x4 &= 0xFFFFFFFF;
@@ -197,33 +207,31 @@ uintptr_t ethosn_smc_handler(uint32_t smc_fid,
 		SMC_RET1(handle, SMC_UNK);
 	}
 
-	if (ETHOSN_STATUS == ETHOSN_STATUS_DISABLED) {
-		WARN("ETHOSN: Arm Ethos-N NPU not available\n");
-		SMC_RET1(handle, ETHOSN_NOT_SUPPORTED);
+	/* Commands that do not require a valid core address */
+	switch (fid) {
+ 	case ETHOSN_FNUM_VERSION:
+ 		SMC_RET2(handle, ETHOSN_VERSION_MAJOR, ETHOSN_VERSION_MINOR);
 	}
 
-	switch (smc_fid & FUNCID_NUM_MASK) {
+	if (!ethosn_is_core_addr_valid(core_addr)) {
+		WARN("ETHOSN: Unknown core address given to SMC call.\n");
+		SMC_RET1(handle, ETHOSN_UNKNOWN_CORE_ADDRESS);
+	}
+
+	/* Commands that require a valid addr */
+	switch (fid) {
 	case ETHOSN_FNUM_VERSION:
 		SMC_RET2(handle, ETHOSN_VERSION_MAJOR, ETHOSN_VERSION_MINOR);
 	case ETHOSN_FNUM_IS_SEC:
-		SMC_RET1(handle, ethosn_is_sec());
+        SMC_RET1(handle, ethosn_is_sec(core_addr));
 	case ETHOSN_FNUM_HARD_RESET:
 		hard_reset = 1;
 		/* Fallthrough */
 	case ETHOSN_FNUM_SOFT_RESET:
-		if (core_idx >= ETHOSN_NUM_CORES) {
-			WARN("ETHOSN: core index out of range\n");
-			SMC_RET1(handle, ETHOSN_CORE_IDX_OUT_OF_RANGE);
-		}
-
-		core_addr = ETHOSN_CORE_ADDR(core_idx);
-
 		if (!ethosn_reset(core_addr, hard_reset)) {
 			SMC_RET1(handle, ETHOSN_FAILURE);
 		}
-
 		ethosn_delegate_to_ns(core_addr);
-
 		SMC_RET1(handle, ETHOSN_SUCCESS);
 	default:
 		SMC_RET1(handle, SMC_UNK);
--- plat/sp/common/sp_ethosn_getter.c
+++ plat/sp/common/sp_ethosn_getter.c
@@ -2,9 +2,8 @@
 #include <sp_ethosn_getter.h>
 
 struct ethosn_config_t ethosn_config={
-	ETHOSN_STATUS_ENABLED,
 	1,
-	{SP_NPU_BASE}
+	{{SP_NPU_BASE}}
 };
 
 
