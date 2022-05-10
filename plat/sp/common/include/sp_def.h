fdb10a7a00236376e6d1dd448579c42d6b5a1ae1
Wells Lu <wellslutw@gmail.com>
2022-05-10 15:03:08 +0800

    [plat][sp][common] Added to output an extra character '\r' if current output character is '\n'.

--- plat/sp/common/include/sp_def.h
+++ plat/sp/common/include/sp_def.h
@@ -9,7 +9,7 @@
 
 #define SP_ARM_COUNTER_CLK_IN		25000000 /* 25MHz */
 
-#define SP_UART0_BAUDRATE		921600
+#define SP_UART0_BAUDRATE		115200
 #define SP_UART0_CLK_IN_HZ		(216 * 1000 * 1000)
 
 #define SP_Q645				0x00000630 /* G0.0 */
