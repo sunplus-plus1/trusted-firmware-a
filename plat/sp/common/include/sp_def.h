b96a4363b4d706fdfef1a3784185e9ecc8952783
Wells Lu <wells.lu@sunplus.com>
2024-06-14 09:29:51 +0800

    Support version B chip.

--- plat/sp/common/include/sp_def.h
+++ plat/sp/common/include/sp_def.h
@@ -12,6 +12,7 @@
 #define SP_UART0_BAUDRATE		115200
 #define SP_UART0_CLK_IN_HZ		(216 * 1000 * 1000)
 
-#define SP_7350				0x00000a30 /* G0.0 (0xf8800000) */
+#define SP_7350A			0x00000a30 /* G0.0 (0xf8800000) */
+#define SP_7350B			0x00000a31 /* G0.0 (0xf8800000) */
 
 #endif /* SP_DEF_H */
